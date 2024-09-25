75dd40c2de5a4e0a345bf8b88b4bcde34d3c60bc
godwinsallah16 <godwinsallah16@gmail.com>
2024-09-25 15:38:52 +0000

    component folder renamed wiht small c

--- pulsify-frontend/src/App.jsx
+++ pulsify-frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
 import LeftNav from './components/LeftNav';
 import MusicPlayer from './components/MusicPlayer';
-import Home from './components/Home';
 
 function App() {
   const [currentSongUrl, setCurrentSongUrl] = useState("");
